a53ec430c3a30b6033ae5b033eb59f5ea79e8e36
Raka Permana <rakapermanaputraa@gmail.com>
2026-02-20 20:50:37 +0700

    chore: removed unused

--- composeApp/src/commonMain/kotlin/com/rakapermanaptr/oase/App.kt
+++ composeApp/src/commonMain/kotlin/com/rakapermanaptr/oase/App.kt
@@ -1,25 +1,9 @@
 package com.rakapermanaptr.oase
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.rakapermanaptr.presentation.TestScreen
 import com.rakapermanaptr.presentation.main.MainScreen
-import org.jetbrains.compose.resources.painterResource
-
-import oase.composeapp.generated.resources.Res
-import oase.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
